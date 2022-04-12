9c8e0b533331c667303fff4b3f9b7c5332223f4b
boyone <boyone.g@gmail.com>
2022-04-12 10:24:36 +0700

    Add web ui

---
+++ ui/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const endPoint = process.env.END_POINT || 'https://localhost/api/greeting';
+
+class App extends Component {
+  greeting = {};
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true, greeting: undefined };
+  }
+
+  componentDidMount() {
+    axios
+      .get(endPoint, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then((response) => {
+        this.setState({ greeting: response.data });
+        this.setState({ isLoading: false });
+      });
+  }
+  render() {
+    const { isLoading, greeting } = this.state;
+
+    if (isLoading) {
+      return <div className="App">Loading...</div>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title" id='message'>{greeting.message}, {this.props.name}</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
